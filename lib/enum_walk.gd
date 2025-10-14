99d4eeb52de6d9073eda1ef1d2ff31e9fafd1da7
seukwonkang <kasworld@gmail.com>
2025-10-14 17:45:44 +0900

    del not lib

--- lib/enum_walk.gd
+++
@@ -1,8 +0,0 @@
-class_name EnumWalk
-
-enum Walk {Off, RightFirst, LeftFirst}
-static func walk2str(a :Walk) -> String:
-	return Walk.keys()[a]
-
-static func next(a :Walk) -> Walk:
-	return (a +1) % Walk.keys().size() as Walk
