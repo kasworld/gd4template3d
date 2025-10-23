219896f7284031402d4ec9fa87b4f54cba90738d
seukwonkang <kasworld@gmail.com>
2025-10-23 18:11:11 +0900

    update Animation3D

--- lib/animation3d.gd
+++ lib/animation3d.gd
@@ -3,11 +3,11 @@ class_name Animation3D
 signal animation_ended(st :Node3D, ani :Dictionary)
 
 var animation_list :Array[Dictionary]
-# {ID,  Node3d, Field(position, rotation) , StartValue, EndValue , StartTick, DurSec } 
+# {Name,  Node3d, Field(position, rotation) , StartValue, EndValue , StartTick, DurSec } 
 
-func start_move(node :Node3D, src_pos :Vector3, dst_pos: Vector3, dur_sec :float) -> Dictionary:
+func start_move(name :String, node :Node3D, src_pos :Vector3, dst_pos: Vector3, dur_sec :float) -> Dictionary:
 	var ani = {
-		"ID" : animation_list.size(),
+		"Name" : name, # for end signal
 		"Node3d" : node, 
 		"Field" : "position",
 		"StartValue" : src_pos, 
@@ -24,11 +24,10 @@ func handle_animation() -> void:
 	for ani in animation_list:
 		var rate = (timenow - ani.StartTick) / ani.DurSec
 		if rate >= 1.0:
-			animation_ended.emit(self, ani)
+			animation_ended.emit(ani.Node3d, ani)
 			continue
 		new_list.append(ani)
 		match ani.Field:
 			"position":
 				ani.Node3d.position = lerp(ani.StartValue, ani.EndValue, rate)
 	animation_list = new_list
-	
