54b568a0a5ed44834694b07b968726ce7000882c
seukwonkang <kasworld@gmail.com>
2025-11-22 14:59:57 +0900

    update AxisArrow3D

--- axis_arrow3d/axis_arrow_3d.gd
+++ axis_arrow3d/axis_arrow_3d.gd
@@ -1,22 +1,31 @@
 extends Node3D
 class_name AxisArrow3D
 
+var arrow_list :Array
+var colors := [Color.RED, Color.GREEN, Color.BLUE]
+var label_list :Array
+var label_text := ["X", "Y", "Z"]
 func _ready() -> void:
-	$ArrowX.set_color(Color.RED)
-	$ArrowY.set_color(Color.GREEN)
-	$ArrowZ.set_color(Color.BLUE)
+	for i in 3:
+		var ar = preload("res://arrow3d/arrow_3d.tscn").instantiate()
+		ar.set_color(colors[i])
+		arrow_list.append(ar)
+		add_child(ar)
+
+		var lb = Label3D.new()
+		lb.text = label_text[i]
+		lb.billboard = BaseMaterial3D.BILLBOARD_ENABLED
+		label_list.append(lb)
+		add_child(lb)
+
+	arrow_list[0].rotation.z = -PI/2
+	#arrow_list[1].rotation.y = PI/2
+	arrow_list[2].rotation.x = PI/2
 
 func set_size(l :float) -> AxisArrow3D:
-	$ArrowX.set_size(l, l/50, l/20, 0.9)
-	$ArrowY.set_size(l, l/50, l/20, 0.9)
-	$ArrowZ.set_size(l, l/50, l/20, 0.9)
-	$ArrowX.position.x = l/2
-	$ArrowY.position.y = l/2
-	$ArrowZ.position.z = l/2
-	$LabelX.position.x = l/2
-	$LabelY.position.y = l/2
-	$LabelZ.position.z = l/2
-	$LabelX.font_size = l * 50
-	$LabelY.font_size = l * 50
-	$LabelZ.font_size = l * 50
+	for i in 3:
+		arrow_list[i].set_size(l, l/50, l/20, 0.9)
+		arrow_list[i].position[i] = l/2
+		label_list[i].position[i] = l/2
+		label_list[i].font_size = l * 50
 	return self
