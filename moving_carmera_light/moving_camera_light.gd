9cf8220515dca8ccd97dbded9fcc43cb8b2b8b92
seukwonkang <kasworld@gmail.com>
2025-11-22 11:22:41 +0900

    update MovingCameraLight

--- moving_carmera_light/moving_camera_light.gd
+++ moving_carmera_light/moving_camera_light.gd
@@ -16,11 +16,17 @@ static func SetCurrentCamera(i :int) -> void:
 	SelfList[CurrentNumber].make_current()
 
 var number :int
-var fov = ClampedFloat.new(75,1,179)
+var fov := ClampedFloat.new(75,1,179)
 
-func init(n :int) -> void:
-	number = n
+func get_camera() -> Camera3D:
+	return $Camera3D
+
+func get_light() -> SpotLight3D:
+	return $SpotLight3D
+
+func _ready() -> void:
 	SelfList.append(self)
+	number = SelfList.size()
 	fov_reset()
 
 func copy_position_rotation(n :Node3D) -> void:
@@ -32,12 +38,15 @@ func _to_string() -> String:
 
 func fov_inc() -> void:
 	$Camera3D.fov = fov.set_up()
+	$SpotLight3D.spot_angle = $Camera3D.fov
 
 func fov_dec() -> void:
 	$Camera3D.fov = fov.set_down()
+	$SpotLight3D.spot_angle = $Camera3D.fov
 
 func fov_reset() -> void:
 	$Camera3D.fov = fov.reset()
+	$SpotLight3D.spot_angle = $Camera3D.fov
 
 func move_around_y(center :Vector3, radius :float, height :float, spd :float = 2.3) -> void:
 	var t := -Time.get_unix_time_from_system() /spd
@@ -53,6 +62,7 @@ func set_center_pos_far(center :Vector3, pos :Vector3, far :float) -> void:
 	position = pos
 	look_at(center)
 	$Camera3D.far = far
+	$SpotLight3D.spot_range = far
 
 func make_current() -> void:
 	$Camera3D.current = true
