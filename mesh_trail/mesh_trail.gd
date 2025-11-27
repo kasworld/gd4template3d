947beb47e2c2b5e30f41cb16580868674d6b57db
seukwonkang <kasworld@gmail.com>
2025-11-27 15:57:48 +0900

    adjust TextMesh size in MeshTrail

--- mesh_trail/mesh_trail.gd
+++ mesh_trail/mesh_trail.gd
@@ -172,8 +172,8 @@ func new_mesh_by_type(mesh_type , r :float) -> Mesh:
 			mesh.top_radius = 0
 		_:
 			mesh = TextMesh.new()
-			mesh.depth = r/4
-			mesh.pixel_size = r / 10
-			mesh.font_size = r*40
+			mesh.depth = r/10
+			mesh.pixel_size = r / 6
+			#mesh.font_size = r*40
 			mesh.text = "%s" % mesh_type
 	return mesh
