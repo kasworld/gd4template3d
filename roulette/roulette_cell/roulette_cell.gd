182f6ceb322df8bafd45a5c901d603d0111ffbb8
seukwonkang <kasworld@gmail.com>
2025-12-04 13:38:57 +0900

    update RouletteCell

--- roulette/roulette_cell/roulette_cell.gd
+++ roulette/roulette_cell/roulette_cell.gd
@@ -22,10 +22,10 @@ func init(각도a :float, 반지름a :float, 깊이a :float, 색깔a :Color, 글
 
 	$"시작선".mesh.size = Vector3(반지름*(선끝비 - 선시작비), 깊이, 깊이 /10)
 	#$"글씨".mesh.font = Global3d.font
-	$"글씨".mesh.depth = 깊이/2
+	$"글씨".mesh.depth = 깊이
 	$"글씨".mesh.horizontal_alignment = HorizontalAlignment.HORIZONTAL_ALIGNMENT_RIGHT
 	$"글씨".rotation = Vector3(-PI/2,0,-PI/2)
-	$"글씨".position = Vector3(0, 깊이/4, 반지름)
+	$"글씨".position = Vector3(0, 깊이/2, 반지름)
 	색깔바꾸기(색깔a)
 	글내용바꾸기(글내용a)
 	칸rad바꾸기(각도a)
@@ -51,7 +51,7 @@ func 글씨크기바꾸기() -> void:
 	$"글씨".mesh.pixel_size = 반지름 *sin(칸rad) *0.05
 
 func 강조상태켜기() -> void:
-	if 강조중:
+	if $AnimationPlayer.is_playing():
 		return
 	강조중 = true
 	#$"글씨".scale.x = 2.0
@@ -63,5 +63,8 @@ func 강조상태끄기() -> void:
 	$AnimationPlayer.play("글씨강조끄기")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "글씨강조":
-		강조상태끄기.call_deferred()
+	match anim_name:
+		"글씨강조":
+			강조상태끄기.call_deferred()
+		"글씨강조끄기":
+			pass
