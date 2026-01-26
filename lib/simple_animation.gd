02084acffdb6db304faabf2c677899589b930d53
seukwonkang <kasworld@gmail.com>
2026-01-27 08:40:18 +0900

    WIP support from to node

--- lib/simple_animation.gd
+++ lib/simple_animation.gd
@@ -3,7 +3,7 @@ class_name SimpleAnimation
 signal animation_ended(st :Node, ani :Dictionary)
 
 var animation_list :Array[Dictionary]
-# {Name,  Node, Field(position, rotation, scale), SubField(0,1,2) , StartValue, EndValue , StartTick, DurSec }
+# {Name,  Node, Field(position, rotation, scale), SubField(0,1,2) , From, To , StartTick, DurSec }
 
 func get_animation_count() -> int:
 	return animation_list.size()
@@ -37,81 +37,81 @@ func find_by_Field(field :String) -> Array[Dictionary]:
 			rtn.append(d)
 	return rtn
 
-func start_move(name :String, node :Node, src_val :Variant, dst_val: Variant, dur_sec :float) -> Dictionary:
+func start_move(name :String, aniNode :Node, from :Variant, to: Variant, dur_sec :float) -> Dictionary:
 	var ani := {
 		"Name" : name, # for end signal
-		"Node" : node,
+		"AniNode" : aniNode,
 		"Field" : "position",
-		"StartValue" : src_val,
-		"EndValue" : dst_val,
+		"From" : from,
+		"To" : to,
 		"StartTick" : Time.get_unix_time_from_system(),
 		"DurSec" : dur_sec,
 	}
 	animation_list.append(ani)
 	return ani
 
-func start_move_subfield(name :String, node :Node, sub_index :int, src_val :Variant, dst_val: Variant, dur_sec :float) -> Dictionary:
+func start_move_subfield(name :String, aniNode :Node, sub_index :int, from :Variant, to: Variant, dur_sec :float) -> Dictionary:
 	var ani := {
 		"Name" : name, # for end signal
-		"Node" : node,
+		"AniNode" : aniNode,
 		"Field" : "position",
 		"SubField" : sub_index,
-		"StartValue" : src_val,
-		"EndValue" : dst_val,
+		"From" : from,
+		"To" : to,
 		"StartTick" : Time.get_unix_time_from_system(),
 		"DurSec" : dur_sec,
 	}
 	animation_list.append(ani)
 	return ani
 
-func start_rotate(name :String, node :Node, src_val :Variant, dst_val: Variant, dur_sec :float) -> Dictionary:
+func start_rotate(name :String, aniNode :Node, from :Variant, to: Variant, dur_sec :float) -> Dictionary:
 	var ani := {
 		"Name" : name, # for end signal
-		"Node" : node,
+		"AniNode" : aniNode,
 		"Field" : "rotation",
-		"StartValue" : src_val,
-		"EndValue" : dst_val,
+		"From" : from,
+		"To" : to,
 		"StartTick" : Time.get_unix_time_from_system(),
 		"DurSec" : dur_sec,
 	}
 	animation_list.append(ani)
 	return ani
 
-func start_rotate_subfield(name :String, node :Node, sub_index :int, src_val :Variant, dst_val: Variant, dur_sec :float) -> Dictionary:
+func start_rotate_subfield(name :String, aniNode :Node, sub_index :int, from :Variant, to: Variant, dur_sec :float) -> Dictionary:
 	var ani := {
 		"Name" : name, # for end signal
-		"Node" : node,
+		"AniNode" : aniNode,
 		"Field" : "rotation",
 		"SubField" : sub_index,
-		"StartValue" : src_val,
-		"EndValue" : dst_val,
+		"From" : from,
+		"To" : to,
 		"StartTick" : Time.get_unix_time_from_system(),
 		"DurSec" : dur_sec,
 	}
 	animation_list.append(ani)
 	return ani
 
-func start_scale(name :String, node :Node, src_val :Variant, dst_val: Variant, dur_sec :float) -> Dictionary:
+func start_scale(name :String, aniNode :Node, from :Variant, to: Variant, dur_sec :float) -> Dictionary:
 	var ani := {
 		"Name" : name, # for end signal
-		"Node" : node,
+		"AniNode" : aniNode,
 		"Field" : "scale",
-		"StartValue" : src_val,
-		"EndValue" : dst_val,
+		"From" : from,
+		"To" : to,
 		"StartTick" : Time.get_unix_time_from_system(),
 		"DurSec" : dur_sec,
 	}
 	animation_list.append(ani)
 	return ani
 
-func start_scale_subfield(name :String, node :Node, sub_index :int, src_val :Variant, dst_val: Variant, dur_sec :float) -> Dictionary:
+func start_scale_subfield(name :String, aniNode :Node, sub_index :int, from :Variant, to: Variant, dur_sec :float) -> Dictionary:
 	var ani := {
 		"Name" : name, # for end signal
-		"Node" : node,
+		"AniNode" : aniNode,
 		"Field" : "scale",
 		"SubField" : sub_index,
-		"StartValue" : src_val,
-		"EndValue" : dst_val,
+		"From" : from,
+		"To" : to,
 		"StartTick" : Time.get_unix_time_from_system(),
 		"DurSec" : dur_sec,
 	}
@@ -122,16 +122,29 @@ func handle_animation() -> void:
 	var timenow := Time.get_unix_time_from_system()
 	for i in animation_list.size():
 		var ani :Dictionary = animation_list.pop_front()
-		if ani.Node == null:
+		if ani.AniNode == null:
 			continue
 		var rate :float = (timenow - ani.StartTick) / ani.DurSec
 		if rate >= 1.0:
 			rate = 1.0
+		var fromValue = ani.From
+		if fromValue is Node:
+			if ani.has("SubField"):
+				fromValue = fromValue[ani.Field][ani.SubField]
+			else:
+				fromValue = fromValue[ani.Field]
+		var toValue = ani.To
+		if toValue is Node:
+			if ani.has("SubField"):
+				toValue = toValue[ani.Field][ani.SubField]
+			else:
+				toValue = toValue[ani.Field]
+
 		if ani.has("SubField"):
-			ani.Node[ani.Field][ani.SubField] = lerp(ani.StartValue, ani.EndValue, rate)
+			ani.AniNode[ani.Field][ani.SubField] = lerp(fromValue, toValue, rate)
 		else:
-			ani.Node[ani.Field] = ani.StartValue.lerp(ani.EndValue, rate)
+			ani.AniNode[ani.Field] = fromValue.lerp(toValue, rate)
 		if rate >= 1.0:
-			animation_ended.emit(ani.Node, ani)
+			animation_ended.emit(ani.AniNode, ani)
 		else:
 			animation_list.push_back(ani)
