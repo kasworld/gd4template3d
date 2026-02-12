50e69c7eb6ff66aa088c406c212079c419d42919
seukwonkang <kasworld@gmail.com>
2026-02-12 11:55:07 +0900

    WIP BattleShooter

--- battle_shooter_3d/bs_obj.gd
+++ battle_shooter_3d/bs_obj.gd
@@ -3,6 +3,29 @@ class_name BSObj
 
 enum Type {Ship, Bullet, Homming, Shield}
 
+## by cabinetsize
+static var SizeRate := {
+	Type.Ship :    0.01,
+	Type.Bullet :  0.01,
+	Type.Homming : 0.01,
+	Type.Shield :  0.01,
+}
+
+## by cabinetsize
+static var SpeedRate := {
+	Type.Ship :    0.1,
+	Type.Bullet :  0.1,
+	Type.Homming : 0.1,
+	Type.Shield :  0.1,
+}
+
+static var LifeSec := {
+	Type.Ship :    10.0,
+	Type.Bullet :  10.0,
+	Type.Homming : 10.0,
+	Type.Shield :  10.0,
+}
+
 var mask_dict := {
 	Type.Ship :    BitFlag.FromPosList2(Type.Ship,Type.Bullet,Type.Homming,Type.Shield),
 	Type.Bullet :  BitFlag.FromPosList2(Type.Ship,Type.Bullet,             Type.Shield),
@@ -22,10 +45,17 @@ func _init(t :Type, tm :BattleShooterTeam) -> void:
 	match type:
 		Type.Ship:
 			$MeshInstance3D.mesh = SphereMesh.new()
+			$MeshInstance3D.mesh.radius = BattleShooter.CabinetSize.x * SizeRate[type]
 		Type.Bullet:
 			$MeshInstance3D.mesh = CapsuleMesh.new()
+			$MeshInstance3D.mesh.radius = BattleShooter.CabinetSize.x * SizeRate[type]
+			$MeshInstance3D.mesh.height = BattleShooter.CabinetSize.x * SizeRate[type] *2
 		Type.Homming:
 			$MeshInstance3D.mesh = TorusMesh.new()
+			$MeshInstance3D.mesh.inner_radius = BattleShooter.CabinetSize.x * SizeRate[type] /2
+			$MeshInstance3D.mesh.outer_radius = BattleShooter.CabinetSize.x * SizeRate[type]
 		Type.Shield:
 			$MeshInstance3D.mesh = SphereMesh.new()
+			$MeshInstance3D.mesh.radius = BattleShooter.CabinetSize.x * SizeRate[type]
 	$MeshInstance3D.mesh.material = MultiMeshShape.make_color_material()
+	$MeshInstance3D.mesh.material.albedo_color = team.color
