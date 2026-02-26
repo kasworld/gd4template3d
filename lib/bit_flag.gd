ff7e28c9a4aa98ef4a674fc09138a8b446e7799e
seukwonkang <kasworld@gmail.com>
2026-02-26 13:41:23 +0900

    move to BitFlag SetArray_ GetArray_

--- lib/bit_flag.gd
+++ lib/bit_flag.gd
@@ -44,3 +44,22 @@ static func MakeFilledPosList(bit_len :int) -> Array[int]:
 
 static func MakeFilledFlags(bit_len :int) -> int:
 	return (1 << bit_len+1) -1
+
+
+## set field value at flag true
+static func SetArray_value_at_flag_true(list: Array, flags :int, field:String, value :Variant) -> void:
+	for i in list.size():
+		if TestByPos(i, flags):
+			list[i][field] = value
+
+## set all bool field by flag value
+static func SetArray_bool_by_flag(list: Array, flags :int, field:String) -> void:
+	for i in list.size():
+		list[i][field] = TestByPos(i, flags)
+
+## bool field only
+static func GetArray_flags_from_bool_field(list: Array, field :String) -> int:
+	var rtn := 0
+	for i in list.size():
+		SetByPos(i, list[i][field])
+	return rtn
--- lib/spot_light_group.gd
+++ lib/spot_light_group.gd
@@ -20,27 +20,9 @@ func _init(arg ) -> void:
 	else:
 		assert(false)
 
-## set field value at flag true
-func set_value_at_flag_true(flags :int, field:String, value :Variant) -> void:
-	for i in light_list.size():
-		if BitFlag.TestByPos(i, flags):
-			light_list[i][field] = value
-
 ## set field by index
 func set_value_at_index(index :int, field:String, value :Variant) -> void:
 	light_list[index][field] = value
-
-## set all bool field by flag value
-func set_bool_by_flag(flags :int, field:String) -> void:
-	for i in light_list.size():
-		light_list[i][field] = BitFlag.TestByPos(i, flags)
-
-## bool field only
-func make_flags_from_bool_field(field :String) -> int:
-	var rtn := 0
-	for i in light_list.size():
-		BitFlag.SetByPos(i, light_list[i][field])
-	return rtn
 
 ## not bool field
 func make_array_from_field(field :String) -> Array:
@@ -52,45 +34,44 @@ func make_array_from_field(field :String) -> Array:
 
 ## flag bit == 1 , set light_energy
 func set_light_energy(v :float, flags :int) -> void:
-	set_value_at_flag_true(flags, "light_energy", v)
+	BitFlag.SetArray_value_at_flag_true(light_list, flags, "light_energy", v)
 
 func set_light_energy_at(i :int, v :float) -> void:
 	set_value_at_index(i, "light_energy", v)
 
 func set_light_color(co :Color, flags :int) -> void:
-	set_value_at_flag_true(flags, "light_color", co)
+	BitFlag.SetArray_value_at_flag_true(light_list,flags, "light_color", co)
 
 func set_light_color_at(i :int, co :Color) -> void:
 	set_value_at_index(i, "light_color", co)
 
 ## flag bit == 1 , set visible to b
 func set_light_on(b :bool, flags :int) -> void:
-	set_value_at_flag_true(flags, "visible", b)
+	BitFlag.SetArray_value_at_flag_true(light_list,flags, "visible", b)
 
 func set_light_on_at(i :int, b :bool) -> void:
 	set_value_at_index(i, "visible", b)
 
 ## all light on/off by flag
 func set_light_on_all(flags :int) -> void:
-	set_bool_by_flag(flags, "visible")
+	BitFlag.SetArray_bool_by_flag(light_list, flags, "visible")
 
 ## flag bit == 1 , set light shadow to b
 func set_light_shadow(b :bool, flags :int) -> void:
-	set_value_at_flag_true(flags, "shadow_enabled", b)
+	BitFlag.SetArray_value_at_flag_true(light_list,flags, "shadow_enabled", b)
 
 func set_light_shadow_at(i :int, b :bool) -> void:
 	set_value_at_index(i, "shadow_enabled", b)
 
 ## all light shadow on/off by flag
 func set_light_shadow_all(flags :int) -> void:
-	set_bool_by_flag(flags, "shadow_enabled")
-
+	BitFlag.SetArray_bool_by_flag(light_list, flags, "shadow_enabled")
 
 func get_light_on_all() -> int:
-	return make_flags_from_bool_field("visible")
+	return BitFlag.GetArray_flags_from_bool_field(light_list,"visible")
 
 func get_light_shadow_all() -> int:
-	return make_flags_from_bool_field("shadow_enabled")
+	return BitFlag.GetArray_flags_from_bool_field(light_list,"shadow_enabled")
 
 func get_light_color_all() -> Array:
 	return make_array_from_field("light_color")
