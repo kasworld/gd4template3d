ff20771db3a73d96e41bacad7a05c261ef271777
seukwonkang <kasworld@gmail.com>
2026-03-05 17:53:33 +0900

    replace init_wire_net with WireNet

--- multi_mesh_shape/multi_mesh_shape.gd
+++ multi_mesh_shape/multi_mesh_shape.gd
@@ -19,30 +19,30 @@ func init_집중선(r :float, start:float, end:float, depth :float, count :int, 
 		set_inst_color(i, co)
 	return self
 
-func init_wire_net(net_size :Vector2, grid_count :Vector2i, wire_radius :float, co :Color, alpha :float = 1.0) -> MultiMeshShape:
-	return init_wire_net2(net_size, grid_count, wire_radius, wire_radius, co, alpha)
-func init_wire_net2(net_size :Vector2, grid_count :Vector2i, wire_width :float, wire_height :float, co :Color, alpha :float = 1.0) -> MultiMeshShape:
-	var wire_count := Vector2i(grid_count.x +1, grid_count.y +1)
-	var pos_shift := -Vector3(net_size.x, net_size.y, 0)/2
-	var 선 := BoxMesh.new()
-	var count := wire_count.x + wire_count.y
-	선.material = make_color_material(alpha)
-	init_with_color_mesh(선, count)
-	for i in count:
-		multimesh.set_instance_color(i,co)
-		if i < wire_count.x:
-			var pos := Vector3( net_size.x/(wire_count.x-1)* i, net_size.y/2, 0) + pos_shift
-			var t := Transform3D(Basis(), pos)
-			#t = t.rotated(Vector3(0,1,0), bar_rot)
-			t = t.scaled_local( Vector3(wire_width,net_size.y,wire_height) )
-			multimesh.set_instance_transform(i,t)
-		else:
-			var pos := Vector3(net_size.x/2, net_size.y/(wire_count.y-1)* (i-wire_count.x), 0) + pos_shift
-			var t := Transform3D(Basis(), pos)
-			#t = t.rotated(Vector3(0,1,0), bar_rot)
-			t = t.scaled_local( Vector3(net_size.x,wire_width,wire_height) )
-			multimesh.set_instance_transform(i,t)
-	return self
+#func init_wire_net(net_size :Vector2, grid_count :Vector2i, wire_radius :float, co :Color, alpha :float = 1.0) -> MultiMeshShape:
+	#return init_wire_net2(net_size, grid_count, wire_radius, wire_radius, co, alpha)
+#func init_wire_net2(net_size :Vector2, grid_count :Vector2i, wire_width :float, wire_height :float, co :Color, alpha :float = 1.0) -> MultiMeshShape:
+	#var wire_count := Vector2i(grid_count.x +1, grid_count.y +1)
+	#var pos_shift := -Vector3(net_size.x, net_size.y, 0)/2
+	#var 선 := BoxMesh.new()
+	#var count := wire_count.x + wire_count.y
+	#선.material = make_color_material(alpha)
+	#init_with_color_mesh(선, count)
+	#for i in count:
+		#multimesh.set_instance_color(i,co)
+		#if i < wire_count.x:
+			#var pos := Vector3( net_size.x/(wire_count.x-1)* i, net_size.y/2, 0) + pos_shift
+			#var t := Transform3D(Basis(), pos)
+			##t = t.rotated(Vector3(0,1,0), bar_rot)
+			#t = t.scaled_local( Vector3(wire_width,net_size.y,wire_height) )
+			#multimesh.set_instance_transform(i,t)
+		#else:
+			#var pos := Vector3(net_size.x/2, net_size.y/(wire_count.y-1)* (i-wire_count.x), 0) + pos_shift
+			#var t := Transform3D(Basis(), pos)
+			##t = t.rotated(Vector3(0,1,0), bar_rot)
+			#t = t.scaled_local( Vector3(net_size.x,wire_width,wire_height) )
+			#multimesh.set_instance_transform(i,t)
+	#return self
 
 func init_bar_gauge_y(count :int, sz :Vector3, co1 :Color, co2 :Color, alpha :float = 1.0, gaprate :float = 0.1) -> MultiMeshShape:
 	var mesh := BoxMesh.new()
