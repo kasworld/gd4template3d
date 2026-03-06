9a3d3e09d176333d007d8b5a848dfe5acbffaa22
seukwonkang <kasworld@gmail.com>
2026-03-06 17:19:59 +0900

    maze floor ceiling tile multi color

--- maze_3d/maze_3d.gd
+++ maze_3d/maze_3d.gd
@@ -92,6 +92,8 @@ func init_floor_ceiling(grid_count :Vector2i, wire_width :float, wire_height :fl
 	#$Floor.tile_rotation_x = PI/2
 	$Floor.rotation.x = -PI/2
 	$Floor.position.y -= calc_grid.unit_size.y/2 +wire_height/2
+	for i in $Floor.get_visible_count():
+		$Floor.set_inst_color(i, NamedColors.random_color())
 
 	$Ceiling.init_tile_grid_with_box(
 		Vector3(net_size.x, net_size.y, wire_height),
@@ -99,6 +101,8 @@ func init_floor_ceiling(grid_count :Vector2i, wire_width :float, wire_height :fl
 	#$Ceiling.tile_rotation_x = PI/2
 	$Ceiling.rotation.x = PI/2
 	$Ceiling.position.y += calc_grid.unit_size.y/2 +wire_height/2
+	for i in $Ceiling.get_visible_count():
+		$Ceiling.set_inst_color(i, NamedColors.random_color())
 	return self
 
 func make_box_pillas() -> void:
