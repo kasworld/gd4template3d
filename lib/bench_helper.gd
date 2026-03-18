11b1ae3e097d8edd94fbdcc21d561d7b306fdb24
seukwonkang <kasworld@gmail.com>
2026-03-18 19:41:08 +0900

    update BenchHelper

--- lib/bench_helper.gd
+++ lib/bench_helper.gd
@@ -5,14 +5,13 @@ var start_time :float
 var dur_sec :float
 var val_per_sec :float
 
-func _init(count_init ) -> void:
-	count = int(count_init)
+func _init() -> void:
 	start_time = Time.get_unix_time_from_system()
 
-func end(new_count :int = count) -> void:
-	count = new_count
+func end(count_a = 0) -> void:
+	count = int(count_a)
 	dur_sec = Time.get_unix_time_from_system() - start_time
 	val_per_sec = count / dur_sec
 
-func get_result() -> String:
+func _to_string() -> String:
 	return "count %d, %f sec, %f count/sec" % [count , dur_sec, val_per_sec ]
