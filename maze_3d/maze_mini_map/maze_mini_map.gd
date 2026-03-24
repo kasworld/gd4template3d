903f3064adeec4970b3f6b04981e36c3c0072159
seukwonkang <kasworld@gmail.com>
2026-03-24 14:09:07 +0900

    set_color MazeMiniMap

--- maze_3d/maze_mini_map/maze_mini_map.gd
+++ maze_3d/maze_mini_map/maze_mini_map.gd
@@ -3,6 +3,9 @@ class_name MazeMiniMap
 
 var maze2d_helper := Maze2DHelper.new()
 var wall_lines_all := WallLines.new()
+var line_color := Color(Color.WHITE,0.5)
+func set_color(co :Color) -> void:
+	line_color = co
 
 func _to_string() -> String:
 	return "MazeMiniMap %s" % maze2d_helper.maze
@@ -11,9 +14,10 @@ func set_maze(mz :Maze) -> void:
 	maze2d_helper.set_maze(mz)
 	wall_lines_all.set_helper(maze2d_helper)
 
+
 func update_size(sz :Vector2) -> void:
 	maze2d_helper.update_size(sz)
 	wall_lines_all.make_all_walllines()
 
 func _draw() -> void:
-	draw_multiline(wall_lines_all.walllines, Color(Color.WHITE,0.5), maze2d_helper.wall_thick)
+	draw_multiline(wall_lines_all.walllines, line_color, maze2d_helper.wall_thick)
