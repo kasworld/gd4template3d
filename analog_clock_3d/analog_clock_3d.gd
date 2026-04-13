c555c114013ffea7a6bfb4094e045c8545d51c75
seukwonkang <kasworld@gmail.com>
2026-04-13 14:14:33 +0900

    update AnalogClock3D

--- analog_clock_3d/analog_clock_3d.gd
+++ analog_clock_3d/analog_clock_3d.gd
@@ -1,8 +1,7 @@
 extends Node3D
 class_name AnalogClock3D
 
-enum BarAlign {None, In,Mid,Out}
-enum NumberType {None, Hour,Minute,Degree}
+enum BarAlign {In, Mid, Out}
 
 var font := preload("res://font/HakgyoansimBareondotumR.ttf")
 
@@ -36,10 +35,9 @@ func init(r :float, d :float, fsize :float, backplane:bool=true) -> AnalogClock3
 	$Donut.rotation.x = -PI/2
 	$BackPlane.visible = backplane
 	make_hands(r, d)
-	make_dial_bar_multi(r*0.88, d, BarAlign.Mid)
-	make_dial_num(r*0.95, d, fsize*0.8, NumberType.Minute)
-	make_dial_num(r*0.8, d, fsize, NumberType.Hour)
-
+	make_dial_line_multi(r*0.88, d, BarAlign.Mid)
+	make_dial_text(r*0.95, d, fsize*0.8, range(0,60,5))
+	make_dial_text(r*0.8, d, fsize, [12,1,2,3,4,5,6,7,8,9,10,11] )
 	return self
 
 func make_hands(r :float, d:float)->void:
@@ -58,7 +56,7 @@ func make_hands(r :float, d:float)->void:
 	$MinuteBase/MinuteHand.rotation.z = PI/2
 	$SecondBase/SecondHand.rotation.z = PI/2
 
-func make_dial_bar_multi(r :float, d:float, align :BarAlign):
+func make_dial_line_multi(r :float, d:float, align :BarAlign):
 	var mesh := BoxMesh.new()
 	mesh.material = MultiMeshShape.MakeMultiMeshColorMaterial()
 	$DialBars.init_with_color_mesh(mesh, 360, 1.0)
@@ -66,16 +64,20 @@ func make_dial_bar_multi(r :float, d:float, align :BarAlign):
 	# Set the transform of the instances.
 	var bar_height := d*0.2
 	var bar_size :Vector3
+	var bar_size_list := [
+		# [ mod int, bar size ]
+		[30, Vector3(r/18,r/180,bar_height)],
+		[6, Vector3(r/24,r/480,bar_height)],
+		[1, Vector3(r/72,r/720,bar_height)],
+	]
 	for i in $DialBars.get_visible_count():
 		var rad := deg_to_rad(i)
 		var bar_center := Vector3(cos(rad)*r, sin(rad)*r,  0)
 		var bar_position := Vector3.ZERO
-		if i % 30 == 0 :
-			bar_size = Vector3(r/18,r/180,bar_height)
-		elif i % 6 == 0 :
-			bar_size = Vector3(r/24,r/480,bar_height)
-		else :
-			bar_size = Vector3(r/72,r/720,bar_height)
+		for bs in bar_size_list:
+			if i % bs[0] == 0:
+				bar_size = bs[1]
+				break
 		match align:
 			BarAlign.In :
 				bar_position = bar_center*(1 - bar_size.x/r/2)
@@ -89,32 +91,16 @@ func make_dial_bar_multi(r :float, d:float, align :BarAlign):
 		t = t.scaled_local( bar_size )
 		$DialBars.multimesh.set_instance_transform(i,t)
 
-func make_dial_num(r :float, d:float, fsize :float, nt :NumberType)->void:
+func make_dial_text(r :float, d:float, fsize :float, text_list :Array)->void:
 	var mat := StandardMaterial3D.new()
 	mat.albedo_color = colors.dial_num
 	var bar_height := d*0.2
-	match nt:
-		NumberType.Hour:
-			for i in range(1,13):
-				var rad := deg_to_rad( i*(360.0/12.0) )
-				var bar_center := Vector3(sin(rad)*r, cos(rad)*r, 0)
-				var t := new_text(fsize, bar_height, mat, "%d" % [i])
-				t.position = bar_center
-				add_child(t)
-		NumberType.Minute:
-			for i in range(0,60,5):
-				var rad := deg_to_rad( i*(360.0/60.0) )
-				var bar_center := Vector3(sin(rad)*r, cos(rad)*r, 0)
-				var t := new_text(fsize, bar_height, mat, "%d" % [i])
-				t.position = bar_center
-				add_child(t)
-		NumberType.Degree:
-			for i in range(0,360,30):
-				var rad := deg_to_rad( i*(360.0/360.0) )
-				var bar_center := Vector3(sin(rad)*r, cos(rad)*r, 0)
-				var t := new_text(fsize, bar_height, mat, "%d" % [i])
-				t.position = bar_center
-				add_child(t)
+	var unit_rad := 2*PI/text_list.size()
+	for i in text_list.size():
+		var rad := i*unit_rad
+		var t := new_text(fsize, bar_height, mat, "%s" % text_list[i])
+		t.position = Vector3(sin(rad)*r, cos(rad)*r, 0)
+		add_child(t)
 
 func new_text(fsize :float, fdepth :float, mat :Material, text :String) -> MeshInstance3D:
 	var mesh := TextMesh.new()
@@ -128,22 +114,17 @@ func new_text(fsize :float, fdepth :float, mat :Material, text :String) -> MeshI
 	return sp
 
 ## ms : Time.get_unix_time_from_system() , tz_shift : 9
-func update_clock(ms :float , tz_shift :float):
-	#var ms := Time.get_unix_time_from_system()
+func update_clock(ms :float, tz_shift :float):
+	var hands_rad := calc_rad_for_hand(ms, tz_shift)
+	$SecondBase.rotation.z = -hands_rad[2]
+	$MinuteBase.rotation.z = -hands_rad[1]
+	$HourBase.rotation.z = -hands_rad[0]
+
+## [hour, minute, second]
+func calc_rad_for_hand(ms :float, tz_shift :float) -> Array[float]:
 	var second := ms - int(ms/60)*60
 	ms = ms / 60
 	var minute := ms - int(ms/60)*60
 	ms = ms / 60
 	var hour := ms - int(ms/24)*24 + tz_shift
-	$SecondBase.rotation.z = -second2rad(second)
-	$MinuteBase.rotation.z = -minute2rad(minute)
-	$HourBase.rotation.z = -hour2rad(hour)
-
-func second2rad(sec :float) -> float:
-	return 2.0*PI/60.0*sec
-
-func minute2rad(m :float) -> float:
-	return 2.0*PI/60.0*m
-
-func hour2rad(hour :float) -> float:
-	return 2.0*PI/12.0*hour
+	return [PI/6.0*hour, PI/30.0*minute, PI/30.0*second]
