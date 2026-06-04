a32090d72a8eb64d600b6077de8a56c1732a9ef6
seukwonkang <kasworld@gmail.com>
2026-06-04 11:24:44 +0900

    add size

--- wire_net/wire_net.gd
+++ wire_net/wire_net.gd
@@ -41,6 +41,7 @@ func start_V_rotate_y(aniname :String, from :float, to :float, dur_sec :float) -
 
 var h_count :int
 var v_count :int
+var size := Vector3.ZERO
 
 func init(net_size :Vector2, grid_count :Vector2i, wire_width :float, wire_height :float, co :Color, transparent :bool = false) -> WireNet:
 	init_wire_H(net_size,grid_count,wire_width,wire_height,co,transparent)
@@ -48,14 +49,15 @@ func init(net_size :Vector2, grid_count :Vector2i, wire_width :float, wire_heigh
 	return self
 
 func init_wire_H(net_size :Vector2, grid_count :Vector2i, wire_width :float, wire_height :float, co :Color, transparent :bool = false) -> WireNet:
-	WireH = preload("res://multi_mesh_shape/multi_mesh_shape.tscn").instantiate()
 	h_count = grid_count.y
+	var unit_y := net_size.y/(h_count-1) if h_count > 1 else 0.0
+	size = Vector3( max(size.x, net_size.x), max(size.y, net_size.y+wire_width), max(size.z, wire_height) )
+	WireH = preload("res://multi_mesh_shape/multi_mesh_shape.tscn").instantiate()
 	var mesh := BoxMesh.new()
 	mesh.size = Vector3(net_size.x, wire_width, wire_height)
 	mesh.material = MultiMeshShape.MakeMultiMeshColorMaterial(transparent)
 	WireH.init_with_color_mesh(mesh, h_count, false)
 	var pos_shift := -Vector3(net_size.x, net_size.y, 0)/2
-	var unit_y := net_size.y/(h_count-1) if h_count > 1 else 0.0
 	for i in h_count:
 		var pos := Vector3(net_size.x/2, unit_y * i, 0) + pos_shift
 		var t := Transform3D(Basis(), pos)
@@ -65,14 +67,15 @@ func init_wire_H(net_size :Vector2, grid_count :Vector2i, wire_width :float, wir
 	return self
 
 func init_wire_V(net_size :Vector2, grid_count :Vector2i, wire_width :float, wire_height :float, co :Color, transparent :bool = false) -> WireNet:
-	WireV = preload("res://multi_mesh_shape/multi_mesh_shape.tscn").instantiate()
 	v_count = grid_count.x
+	var unit_x := net_size.x/(v_count-1) if v_count > 1 else 0.0
+	size = Vector3( max(size.x, net_size.x+wire_width), max(size.y, net_size.y), max(size.z, wire_height) )
+	WireV = preload("res://multi_mesh_shape/multi_mesh_shape.tscn").instantiate()
 	var mesh := BoxMesh.new()
 	mesh.size = Vector3(wire_width, net_size.y, wire_height)
 	mesh.material = MultiMeshShape.MakeMultiMeshColorMaterial(transparent)
 	WireV.init_with_color_mesh(mesh, v_count, false)
 	var pos_shift := -Vector3(net_size.x, net_size.y, 0)/2
-	var unit_x := net_size.x/(v_count-1) if v_count > 1 else 0.0
 	for i in v_count:
 		var pos := Vector3( unit_x * i, net_size.y/2, 0) + pos_shift
 		var t := Transform3D(Basis(), pos)
