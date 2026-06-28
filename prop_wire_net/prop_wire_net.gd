dfa943585dd9c3b0a5a7f237083ce6e02c0b9660
seukwonkang <kasworld@gmail.com>
2026-06-28 13:01:16 +0900

    WIP PropWireNet

--- prop_wire_net/prop_wire_net.gd
+++ prop_wire_net/prop_wire_net.gd
@@ -1,6 +1,8 @@
 extends Node3D
 class_name PropWireNet
 
+## x-y wire net, face z+
+
 static func make_box(box_size :Vector3, box_mat :StandardMaterial3D) -> CSGShape3D:
 	var box := CSGBox3D.new()
 	box.size = box_size
@@ -13,12 +15,32 @@ static func MakeColorMaterial(co :Color, transparent :bool = false) -> StandardM
 	material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA if transparent else BaseMaterial3D.TRANSPARENCY_DISABLED
 	return material
 
-static func MakeCSG() -> CSGShape3D:
+static func MakeCSG(net_size :Vector2, grid_count :Vector2i, wire_width :float, wire_height :float, co :Color, transparent :bool = false) -> CSGShape3D:
+	var mat := MakeColorMaterial(co,transparent)
 	var center := make_box(Vector3.ONE, MakeColorMaterial(Color(0,0,0,0), true) )
+	# make H wire
+	var h_wire_size := Vector3(net_size.x,wire_width, wire_height)
+	var unit_y := net_size.y/(grid_count.y-1) if grid_count.y > 1 else 0.0
+	for i in grid_count.y:
+		var pos := Vector3(0, -net_size.y/2 + unit_y *i, 0)
+		var wire := make_box(h_wire_size, mat)
+		wire.operation = CSGShape3D.OPERATION_UNION
+		wire.position = pos
+		center.add_child(wire)
+
+	var v_wire_size := Vector3(wire_width, net_size.y, wire_height)
+	var unit_x := net_size.x/(grid_count.x-1) if grid_count.x > 1 else 0.0
+	for i in grid_count.x:
+		var pos := Vector3(-net_size.x/2 + unit_x *i, 0, 0)
+		var wire := make_box(v_wire_size, mat)
+		wire.operation = CSGShape3D.OPERATION_UNION
+		wire.position = pos
+		center.add_child(wire)
+
 	return center
 
-func init(top_size :Vector3, leg_size :Vector3, co_top :Color, co_leg :Color) -> PropWireNet:
-	var wall := MakeCSG()
+func init(net_size :Vector2, grid_count :Vector2i, wire_width :float, wire_height :float, co :Color, transparent :bool = false) -> PropWireNet:
+	var wall := MakeCSG(net_size,grid_count,wire_width, wire_height, co, transparent)
 	bake.call_deferred(wall)
 	return self
 
