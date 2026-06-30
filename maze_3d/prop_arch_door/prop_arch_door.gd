9ed2e1da292be533eb174608d7058463c6df416a
seukwonkang <kasworld@gmail.com>
2026-06-30 16:38:45 +0900

    use csg lib PropArchDoor

--- maze_3d/prop_arch_door/prop_arch_door.gd
+++ maze_3d/prop_arch_door/prop_arch_door.gd
@@ -3,7 +3,7 @@ class_name PropArchDoor
 
 ## face Z+ wall with door
 func init(size :Vector3, co :Color) -> PropArchDoor:
-	var wall := MakeArchDoorMeshH(size,MakeColorMaterial(co))
+	var wall := MakeArchDoorMeshH(size,CSG.MakeColorMaterial(co,false))
 	bake.call_deferred(wall)
 	return self
 
@@ -12,19 +12,11 @@ func bake(csg :CSGShape3D) -> void:
 
 ## face Z+ wall with door
 static func MakeArchDoorMeshH(size :Vector3, mat :StandardMaterial3D) -> CSGShape3D:
-	var wall := CSGBox3D.new()
-	wall.size = size
-	wall.material = mat
-	var door_low := CSGBox3D.new()
-	door_low.size = Vector3(size.x/2,size.y/2,size.z)
-	door_low.material = mat
+	var wall := CSG.MakeCSGBox(size,mat)
+	var door_low := CSG.MakeCSGBox(Vector3(size.x/2,size.y/2,size.z),mat)
 	door_low.operation = CSGShape3D.OPERATION_SUBTRACTION
 	door_low.position.y = -size.y * 0.25
-	var hole := CSGCylinder3D.new()
-	hole.material = mat
-	hole.radius = size.x/4
-	hole.height = size.z *2
-	hole.sides = 64
+	var hole := CSG.MakeCSGCylinder(size.x/4,size.z *2,mat,64)
 	hole.rotate_x(PI/2)
 	hole.operation = CSGShape3D.OPERATION_SUBTRACTION
 	wall.add_child(door_low)
@@ -33,27 +25,13 @@ static func MakeArchDoorMeshH(size :Vector3, mat :StandardMaterial3D) -> CSGShap
 
 ## face X+ wall with door
 static func MakeArchDoorMeshV(size :Vector3, mat :StandardMaterial3D) -> CSGShape3D:
-	var wall := CSGBox3D.new()
-	wall.size = size
-	wall.material = mat
-	var door_low := CSGBox3D.new()
-	door_low.size = Vector3(size.x,size.y/2,size.z/2)
-	door_low.material = mat
+	var wall := CSG.MakeCSGBox(size,mat)
+	var door_low := CSG.MakeCSGBox(Vector3(size.x,size.y/2,size.z/2),mat)
 	door_low.operation = CSGShape3D.OPERATION_SUBTRACTION
 	door_low.position.y = -size.y * 0.25
-	var hole := CSGCylinder3D.new()
-	hole.material = mat
-	hole.radius = size.z/4
-	hole.height = size.x *2
-	hole.sides = 64
+	var hole := CSG.MakeCSGCylinder(size.z/4,size.x *2,mat,64)
 	hole.rotate_z(PI/2)
 	hole.operation = CSGShape3D.OPERATION_SUBTRACTION
 	wall.add_child(door_low)
 	wall.add_child(hole)
 	return wall
-
-static func MakeColorMaterial(co :Color, transparent :bool = false) -> StandardMaterial3D:
-	var material := StandardMaterial3D.new()
-	material.albedo_color = co
-	material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA if transparent else BaseMaterial3D.TRANSPARENCY_DISABLED
-	return material
